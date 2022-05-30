4888ec823d8d240de0d6dd0a48fffbb94484d42d
Freppan <fredrik.hagslatt@gmail.com>
2022-05-31 00:00:30 +0200

    init commit

---
+++ src/Game.cpp
@@ -0,0 +1,72 @@
+#include "Game.h"
+#include <SDL2/SDL.h>
+#include <iostream>
+
+Game::Game()
+{
+    std::cout << "Game constructor called!" << std::endl;
+}
+
+Game::~Game()
+{
+    std::cout << "Game deconstructor called!" << std::endl;
+}
+
+void Game::Initialize()
+{
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+    // TODO: Create a window
+    SDL_Window* window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED,
+        800,
+        600,
+        SDL_WINDOW_BORDERLESS);
+    if (!window)
+    {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    if(!renderer)
+    {
+        std::cerr << "Error creating SDL renderer." << std::endl;
+    }
+}
+
+void Game::Run()
+{
+/*
+    while(true)
+    {
+        ProcessInput();
+        Update();
+        Render();
+    }
+*/
+}
+
+void Game::ProcessInput()
+{
+
+}
+
+void Game::Update()
+{
+
+}
+
+void Game::Render()
+{
+
+}
+
+void Game::Destroy()
+{
+
+}
